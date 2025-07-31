4d52a98af3153fdd0aded4158c9d7892e0628888
hamster624 <viktors.jirgen@gmail.com>
2025-07-31 13:01:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ from dotenv import load_dotenv
 import os
 import time
 import json
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
 # ---------------------
 # Bot Setup
 # ---------------------
